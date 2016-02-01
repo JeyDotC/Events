3af35ac3dc2bada16baeb9ffdb5712e7c2ef43df
Jeysson José Guevara Mendivil <jeisson0605022@hotmail.com>
2016-01-31 22:39:44 -0500

    Publish on Nuget

--- Events/Properties/AssemblyInfo.cs
+++ Events/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Events")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A small library which purpose is to give a very simple implementation of the observer pattern")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("JeyDotC")]
 [assembly: AssemblyProduct("Events")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © JeyDotC 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
