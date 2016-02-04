135d795168a7b8323e8c4ad5097207060e963388
JeyDotC <jeissonjgm@gmail.com>
2016-02-04 16:43:11 -0500

    Tests for cached Observerrs storage

--- Events.Tests/AssemblyScanningTests.cs
+++ Events.Tests/AssemblyScanningTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -38,5 +39,28 @@ namespace Events.Tests
 
             Assert.IsTrue(Constants.ExpectedResultForHelloWorldEvent.IsMatch(evnt.Data));
         }
+
+        [TestMethod]
+        public void CachedTest()
+        {
+            var cache = new MemoryCache("MyObservers");
+            var emitter = new EventEmitter(new CachedObserverStorage(cache, _observers));
+            var evnt = new HelloWorldEvent();
+
+            var noncachedStartTicks = DateTime.Now.Ticks;
+            emitter.Emit(evnt);
+            var nonCachedTicks = DateTime.Now.Ticks - noncachedStartTicks;
+
+            var cachedStartTicks = DateTime.Now.Ticks;
+            emitter.Emit(evnt);
+            var cachedTicks = DateTime.Now.Ticks - cachedStartTicks;
+
+            Console.WriteLine("Difference in miliseconds between cached and noncached: {0}", (nonCachedTicks - cachedTicks) / TimeSpan.TicksPerMillisecond);
+
+            Assert.AreNotEqual(cachedTicks, nonCachedTicks);
+            Assert.IsTrue(cachedTicks < nonCachedTicks);
+
+        }
+
     }
 }
